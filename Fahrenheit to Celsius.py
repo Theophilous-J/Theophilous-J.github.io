c2732f42a722b05e05245d57b0d25ce760093320
Theophilous-J <jpt4563@gmail.com>
2024-06-17 20:31:47 -0400

    Add files via upload

---
+++ Fahrenheit to Celsius.py
@@ -0,0 +1,13 @@
+#Jesse Theophilous
+#Temps lab
+
+#math
+C= float(input("What is the temperature outside in Celcius?: "))
+F= float(1.8*C+ 32)
+
+
+
+#Displays C and F
+print("Celsius  Fahrenheit")
+print(C, "     ", F)
+
